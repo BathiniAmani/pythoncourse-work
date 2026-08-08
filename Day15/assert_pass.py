a83f8f2d860fa8b9ac1599731b7d9071781e01b5
Amani Bathini <amanibathini28@gmail.com>
2026-08-08 10:31:26 +0530

    Add files via upload

---
+++ Day15/assert_pass.py
@@ -0,0 +1,23 @@
+#pass is to define empty block of code
+#assert is used to debug purpose
+#debugging: finding the errors
+
+'''
+n = int(input())
+for i in range(n):
+  pass
+'''
+
+'''
+age = 20
+
+assert age >= 18
+
+print("Eligible to vote")
+'''
+
+age = 15
+
+assert age >= 18
+
+print("Eligible to vote")
